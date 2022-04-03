b9984bea54ce07c6abbf75628447764b2dcf0fa7
Sascha Dziuballe <sascha@dziuballe.eu>
2022-04-03 11:03:20 +0200

    created random() function instead of calling Math.floor(Math.random()) in every createPassword function

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ const umlauts = "ÄäÖöÜüßáàâéèêíìîóòû";
 const specialChars = "@€>^°!§$%&()=?{[]}+*~#-_+";
 var result = document.getElementById("password");
 
+
+function random(x) {
+    r = Math.floor(Math.random() * (x));
+    return r;
+}
+
 function createPasswordSimple() {
     const seed = lowerCaseLetters + upperCaseLetters + numbers;
     var pw = "";
     for (i=0;i<8;i++) {
-        var char = Math.floor(Math.random() * (seed.length));
+        var char = random(seed.length);
         pw += seed[char];
     };
     result.innerHTML += "<p>" + pw + "</p>";
@@ -19,7 +25,7 @@ function createPasswordComplex() {
     const seed = lowerCaseLetters + lowerCaseLetters + upperCaseLetters + upperCaseLetters + numbers + umlauts;
     var pw = "";
     for (i=0;i<16;i++) {
-        var char = Math.floor(Math.random() * (seed.length));
+        var char = random(seed.length);
         pw += seed[char];
     };
     result.innerHTML += "<p>" + pw + "</p>";
@@ -29,7 +35,7 @@ function createPasswordExtreme() {
     const seed = lowerCaseLetters + lowerCaseLetters + upperCaseLetters + upperCaseLetters + numbers + umlauts + specialChars;
     var pw = "";
     for (i=0;i<32;i++) {
-        var char = Math.floor(Math.random() * (seed.length));
+        var char = random(seed.length);
         pw += seed[char];
     };
     result.innerHTML += "<p>" + pw + "</p>";
