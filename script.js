6c97e71dde2c6b0e8f976e40a49bde2893b6c387
KiofKi <sascha_kiofki@yahoo.de>
2022-04-06 09:27:47 +0200

    improved seeds and pw generation

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const vowels = "aeiouy";
+const consonants = "bcdfghjklmnpqrstvwxz"
 const numbers = "0123456789";
-const umlauts = "ÄäÖöÜüßáàâéèêíìîóòû";
 const specialChars = "!$%()=?+#-.:~*@[]_";
-const simpleSeed = lowerCaseLetters + upperCaseLetters + numbers
-const complexSeed = lowerCaseLetters + upperCaseLetters + numbers + specialChars;
-const extremeSeed = lowerCaseLetters + upperCaseLetters + numbers + umlauts + specialChars;
 var result = document.getElementById("password");
 
 
@@ -16,28 +12,40 @@ function random(x) {
 
 function createPasswordSimple() {
     var pw = "";
-    for (i=0;i<10;i++) {
-        var char = random(simpleSeed.length);
-        pw += simpleSeed[char];
+    seed1 = vowels + vowels.toUpperCase();
+    seed2 = consonants + consonants.toUpperCase();
+    for (i=0;i<4;i++) {
+        pw += seed1[random(seed1.length)];
+        pw += seed2[random(seed2.length)];
     };
+    for (i=0;i<2;i++) {
+        pw+= numbers[random(numbers.length)];
+    }
     result.innerHTML += `<p>${pw}</p>`;
 };
 
 function createPasswordComplex() {
     var pw = "";
-    for (i=0;i<20;i++) {
-        var char = random(complexSeed.length);
-        pw += complexSeed[char];
+    var seed = vowels + vowels.toUpperCase() + consonants + consonants.toUpperCase() + numbers;
+    for (i=0;i<8;i++) {
+        pw += seed[random(seed.length)];
     };
+    pw += specialChars[random(specialChars.length)];
+    for (i=0;i<7;i++) {
+        pw += seed[random(seed.length)];
+    }
     result.innerHTML += `<p>${pw}</p>`;
 };
 
 function createPasswordExtreme() {
     var pw = "";
-    for (i=0;i<32;i++) {
-        var char = random(extremeSeed.length);
-        pw += extremeSeed[char];
+    seed1 = vowels + vowels.toUpperCase() + consonants + consonants.toUpperCase();
+    seed2 = vowels + vowels.toUpperCase() + consonants + consonants.toUpperCase() + numbers + specialChars;
+    pw += seed1[random(seed1.length)];
+    for (i=0;i<30;i++) {
+        pw += seed2[random(seed2.length)];
     };
+    pw += seed1[random(seed1.length)];
     result.innerHTML += `<p>${pw}</p>`;
 };
 
